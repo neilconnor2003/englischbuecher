17be74a133f1ed0470fe33d4d0ce975dd3edcee2
Neil Chatterjee <neilconnor2003@gmail.com>
2026-03-31 12:11:39 +0200

    Fix Contact page design

--- frontend/src/pages/FooterPages/Contact.jsx
+++ frontend/src/pages/FooterPages/Contact.jsx
@@ -1,5 +1,5 @@
 // frontend/src/pages/Contact.jsx
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useGetContactQuery } from '../../admin/features/contact/contactApiSlice';
 import './Contact.css';
